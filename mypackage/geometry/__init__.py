8c39e9fac36df93e69fa823bc5a9b595a254e6f7
JulianHeymes <julian.heymes@gmail.com>
2025-05-08 11:35:40 +0200

    Added ruff into pre-commit-config, and corrected all errors by modifying both __init__.py, geometry.py and utils.py can be removed

--- mypackage/geometry/__init__.py
+++ mypackage/geometry/__init__.py
@@ -1,1 +1,8 @@
 from .geometry import Point, Line
+
+
+__all__ = [
+    "Point",
+    "Line",
+    "distance",
+]  # Directly importable now in the package -- changed after ruff complained
--- mypackage/geometry/geometry.py
+++ mypackage/geometry/geometry.py
@@ -1,9 +1,11 @@
-from ..utils import distance
+# from ..utils import distance
+
 
 class Point:
     def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
+
 
 class Line:
     def __init__(self, p1: "Point", p2: "Point") -> None:
@@ -12,3 +14,7 @@ class Line:
 
     def length(self) -> float:
         return distance(self.p1, self.p2)
+
+
+def distance(p1: "Point", p2: "Point") -> float:
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
